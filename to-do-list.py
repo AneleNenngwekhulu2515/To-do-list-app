9de3bbf9b9ae6ec7707e1c8a6f0e6e0115cabe5f
Anele Nenngwekhulu <annenngjhb024@student.wethinkcode.co.za>
2025-03-28 10:31:05 +0200

    imports

--- to-do-list.py
+++ to-do-list.py
@@ -4,3 +4,10 @@ import os
 
 tasks = []
 FILE_NAME = "tasks.txt"
+
+def load_tasks():
+    if os.path.exists(FILE_NAME):
+        with open (FILE_NAME, "r") as file:
+            for line in file:
+                tasks.append(line.strip())
+                listbox.insert(tk.end, line.strip())
